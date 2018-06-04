d8d150986f43cdc2325fbeef180361505bd971f3
ThiagoVSouza <35876587+ThiagoVSouza@users.noreply.github.com>
2018-06-04 16:35:09 -0300

    a

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+var net = require('net');
 
+var server = net.createServer(function(socket) {
+
+	socket.write('Echo server\r\n');
+    socket.pipe(socket);
+    
+});
+
+server.listen(1337, '127.0.0.1');
+
+
+
+
+
+/*
 
 var net = require('net');
 
@@ -14,6 +29,8 @@ var server = net.createServer(function(socket) {
 });
 
 server.listen(5000);
+
+*/
 
 /*
 var http = require('http');
